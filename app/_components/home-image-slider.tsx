747d9719a18cf391255f9301a8ddeb68ece2c813
Jovan Lanutan <jovan.lanutan@cwti.local>
2025-05-12 11:06:12 +0800

    Add and optimized image, fix navigation carousel

--- app/_components/home-image-slider.tsx
+++ app/_components/home-image-slider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Autoplay from "embla-carousel-autoplay";
 import { useState } from "react";
 import {
@@ -12,13 +13,22 @@ import {
 
 const images = [
   {
-    link: "Image 01", // replace it with path for images
+    link: "/img/home/slider/ph365-image-slide-01.webp",
   },
   {
-    link: "Image 02",
+    link: "/img/home/slider/ph365-image-slide-02.webp",
   },
   {
-    link: "Image 03",
+    link: "/img/home/slider/ph365-image-slide-03.webp",
+  },
+  {
+    link: "/img/home/slider/ph365-image-slide-04.webp",
+  },
+  {
+    link: "/img/home/slider/ph365-image-slide-05.webp",
+  },
+  {
+    link: "/img/home/slider/ph365-image-slide-06.webp",
   },
 ];
 
@@ -27,18 +37,23 @@ export default function Slider() {
     Autoplay({ delay: 3000, stopOnInteraction: true })
   );
   return (
-    <Carousel plugins={[autoplay]} className="py-2 lg:py-4">
+    <Carousel plugins={[autoplay]} className="group py-2 lg:py-4">
       <CarouselContent className="w-full h-[150px] md:[h-250px] lg:w-[1245px] lg:h-[300px]">
         {images.map((image, index) => (
           <CarouselItem key={index}>
-            <div className="aspect-square h-full w-full flex items-center justify-center border border-dashed border-dark/20 rounded-md text-dark">
-              {image.link}
+            <div className="aspect-square h-full w-full flex items-center justify-center border border-dashed border-dark/20 rounded-lg cursor-pointer text-dark">
+              <Image
+                src={image.link}
+                width={1245}
+                height={300}
+                alt={image.link}
+              />
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="hidden lg:block" />
-      <CarouselNext className="hidden lg:block" />
+      <CarouselPrevious className="hidden cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity lg:block" />
+      <CarouselNext className="hidden cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity lg:block" />
     </Carousel>
   );
 }
